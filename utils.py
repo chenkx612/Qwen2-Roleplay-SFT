8afbaf4cc2bb70cf97492491046e4c8a69b9e64f
chenkx612 <chenkx612@gmail.com>
2025-08-25 12:43:15 +0800

    apply chat template

--- utils.py
+++ utils.py
@@ -1,11 +1,28 @@
 # utils.py
-import torch
 
-def make_prompt(example, prompt_tmpl):
-    return prompt_tmpl.format(instruction=example.get('instruction', ''), input=example.get('input', ''))
+def total_length(sample):
+    return (
+        len(sample['instruction']) + 
+        len(sample['input']) + 
+        len(sample['output'])
+    )
 
-def get_device_of_model(m):
-    try:
-        return next(m.parameters()).device
-    except StopIteration:
-        return torch.device('cpu')
+def print_trainable_parameters(model):
+    trainable = 0
+    total = 0
+    for _, param in model.named_parameters():
+        num = param.numel()
+        total += num
+        if param.requires_grad:
+            trainable += num
+    print(f'可训练参数: {trainable} / {total} ({100*trainable/total:.2f}%)')
+
+def find_sublist(hay, needle):
+    if not needle:
+        return -1
+    Lh = len(hay)
+    Ln = len(needle)
+    for i in range(Lh - Ln, -1, -1):
+        if hay[i:i+Ln] == needle:
+            return i
+    return -1
